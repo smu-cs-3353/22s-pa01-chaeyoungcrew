d4e474540dbd9137fe604e0d3e962f3b10d125cb
Cry1is <zachary.suzuki@gmail.com>
2022-01-23 15:57:11 -0600

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Zachary on 1/23/2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc == 1)
+        cout << "No arguments provided." << endl;
+    else {
+        cout << "Hello World!" << endl;
+    }
+    return 0;
+}
