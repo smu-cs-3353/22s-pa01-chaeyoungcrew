a52c8ea3b5b08ef0a2657ebf53ca0992e791dbf2
Cry1is <zachary.suzuki@gmail.com>
2022-01-31 11:20:06 -0600

    Normalized Art.h and Art.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -5,10 +5,16 @@
 #include "catch.hpp"
 
 // INCLUDE CLASSES HERE
+#include "Art.h"
+#include "Wall.h"
+#include "Museum.h"
 
 /**
  * Create a new test case for every new class.
  */
-TEST_CASE() {
-
+TEST_CASE("Art Test", "[Art]") {
+    SECTION("Art Constructors") {
+        Art def;
+        Art over(0,5,10,10);
+    }
 }
