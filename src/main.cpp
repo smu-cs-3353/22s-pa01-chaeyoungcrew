0c85df38755416631a1f67507c2fb155c0b30343
Cry1is <zachary.suzuki@gmail.com>
2022-02-07 15:16:14 -0600

    cleaning up code

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,9 @@ int main(int argc, char** argv) {
     if (argc == 1)
         cout << "No arguments provided." << endl;
     else {
-        Museum m;
-        /*m.readFile(argv[1]);
-        m.getWall();
-        m.getList();
-        m.getArt(12);
-        cout << m << std::endl;
-        cout << m.getWall() << std::endl;
-        cout << m.getArt(12) << std::endl;*/
-
         Algorithm a(argv[1]);
         cout << "Brute Force: $" << a.bruteForce() << endl;
         cout << "Most Expensive First: $" << a.mostExpensiveFirst() << endl;
-
     }
     return 0;
 }
